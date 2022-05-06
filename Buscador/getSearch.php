8b2220b2a460a5f4abff6c18c46f25aee9d66db8
AlvinPech <77123516+AlvinPech@users.noreply.github.com>
2022-05-06 08:15:31 -0500

    Add files via upload

---
+++ Buscador/getSearch.php
@@ -0,0 +1,17 @@
+<?php
+
+function getSearch($string, $filters){
+  $string = str_replace(" ", "/", $string);
+  $url1 = 'http://localhost:8983/solr/solrhelp/select?rows=100&fl=*%2Cscore&q=';
+  $url2 = '&facet=true&facet.mincount=1&facet.field=title&f.title.facet.limit=5&f.title.facet.sort=count';
+  $people_json = file_get_contents($url1.$string.$url2.$filters);
+  if (!$people_json) {
+    //Si no puede ser expandido
+    $people_json = file_get_contents($url1.$string.$url2.$filters);
+  }
+  $decoded_json = json_decode($people_json, true);
+
+  return $decoded_json;
+
+}
+?>
